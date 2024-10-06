b60a8771def9ba298841d7f1177d96c463c90a37
Abhik Patra <theabhik2020@gmail.com>
2024-10-06 13:34:22 +0530

    final update

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const corsOptions = {
     optionsSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
